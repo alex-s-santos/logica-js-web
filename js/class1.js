811479e2291184f1a492db512e654f769f859b07
alex-s-santos <alexsantos.alss.13@gmail.com>
2025-08-08 21:01:49 -0300

    finaliza aula 1

--- js/class1.js
+++
@@ -1,5 +0,0 @@
-//let name = document.querySelector('#name').value;
-let btn1 = document.querySelector('#btn1');
-let return1 = document.querySelector('#return1');
-
-return1.innerHTML = 'ok'
--- js/classes.js
+++ js/classes.js
@@ -6,12 +6,23 @@ class1.addEventListener('click', function() {
 
     <h3>Seja bem-vindo, digite seu nome</h3>
     <label for="name">Nome: </label>
-    <input type="text" id="name">
+    <input type="text" id="name1">
     <button class="btn" id="btn1">Executar</button>
     <br>
-    <p id="return1">teste</p>
+    <p id="return1" class="return"></p>
     <br>
     <a href="./index.html" class="home">Voltar</a>
-    <script src="./js/class1.js"></script>
    `
+
+   //Lógica
+   let btn1 = document.querySelector("#btn1");
+   let name = document.querySelector("#name1");
+   let return1 = document.querySelector("#return1");
+
+   btn1.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      return1.innerHTML = `Olá, ${name.value}, seja bem-vindo ao nosso curso!`;
+   });
+
 });
