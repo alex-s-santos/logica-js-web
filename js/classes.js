61110a8eb368c7ad4c7cdc11eaaa5d08421075e5
alex-s-santos <alexsantos.alss.13@gmail.com>
2025-08-08 22:33:12 -0300

    Inicia aula 2

--- js/classes.js
+++ js/classes.js
@@ -1,5 +1,6 @@
 let classes = document.querySelector('#classes');
 let class1 = document.querySelector('#class1');
+let class2 = document.querySelector('#class2');
 
 class1.addEventListener('click', function() {
    classes.innerHTML = `
@@ -19,11 +20,42 @@ class1.addEventListener('click', function() {
    let btn1 = document.querySelector("#btn1");
    let name = document.querySelector("#name1");
    let return1 = document.querySelector("#return1");
+   name.focus();
 
    btn1.addEventListener('click', function(e) {
       e.preventDefault();
 
       return1.innerHTML = `Olá, ${name.value}, seja bem-vindo ao nosso curso!`;
+      name.value = "";
+      name.focus();
    });
 
 });
+
+class2.addEventListener('click', function() {
+   classes.innerHTML = `
+
+    <h3>Digite os números e selecione uma operação matemática</h3>
+    <input type="number" id="name1" class="input-num">
+    <select>
+      <option></option>
+      <option>+</option>
+      <option>-</option>
+      <option>x</option>
+      <option>÷</option>
+      <option>X²</option>
+      <option>mod</option>
+    </select>
+    <input type="number" id="name1" class="input-num">
+    <button class="btn" id="btn1">Executar</button>
+    <br>
+    <p id="return1" class="return"></p>
+    <br>
+    <a href="./index.html" class="home">Voltar</a>
+    <br>
+   `
+
+   //Lógica
+
+});
+
